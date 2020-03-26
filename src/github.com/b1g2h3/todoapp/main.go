652facfe3160d115b237a7efc670fd7c1df80438
b1g2h3 <b1g2h3@seznam.cz>
2020-03-26 19:45:57 +0100

    template + barear token + oauth2 pres google

--- src/github.com/b1g2h3/todoapp/main.go
+++ src/github.com/b1g2h3/todoapp/main.go
@@ -3,30 +3,36 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
+	"text/template"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
 // Client
 var client *firestore.Client
+var app *firebase.App
 
 // initilaze firestore Client
 func init() {
 	// Use the application default credentials.
 	ctx := context.Background()
-	sa := option.WithCredentialsFile("C:/Users/vlast/Desktop/credentionals.json")
-
-	app, err := firebase.NewApp(ctx, nil, sa)
+	opt := option.WithCredentialsFile("C:/Users/vlast/Desktop/credentionals.json")
+	config := &firebase.Config{ProjectID: "todo-3840c"}
+	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
-		log.Fatalf("firebase.NewApp: %v", err)
+		log.Fatalf("error initializing app: %v\n", err)
 	}
 
 	client, err = app.Firestore(ctx)
@@ -48,7 +54,77 @@ type Task struct {
 	ListID int    `json:"ListID"`
 }
 
-var lists []Task
+type idToken struct {
+	Value string `json:"Token"`
+}
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     "25387032102-ql5jr8rkasa64nbg3ejf7ojjhv12g7ln.apps.googleusercontent.com",
+		ClientSecret: "kxYYKu30m7tEY6mk0TCduLY9",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	tpl              *template.Template
+	lists            []Task
+	oauthStateString = "pseudo-random"
+)
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func authTest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	content, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Content: %s\n", content)
+}
+
+func getUserInfo(state string, code string) ([]byte, error) {
+	if state != oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	return contents, nil
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+func logout(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
 
 // Get all Lists
 func getLists(w http.ResponseWriter, r *http.Request) {
@@ -62,6 +138,8 @@ func getLists(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatalf("Failed to iterate: %v", err)
 		}
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(doc.Data())
 	}
@@ -102,10 +180,6 @@ func getTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	TaskID, err := strconv.Atoi(params["TaskID"])
-	if err != nil {
-		http.Redirect(w, r, "/login", http.StatusFound)
-		return
-	}
 	iter := client.Collection("task").Where("ListID", "==", ListID).Where("ID", "==", TaskID).Documents(ctx)
 	for {
 		doc, err := iter.Next()
@@ -192,6 +266,11 @@ func handler() {
 	r := mux.NewRouter()
 
 	// Route handles & endpoints
+	r.HandleFunc("/", index)
+	r.HandleFunc("/login", login)
+	r.HandleFunc("/logout", logout)
+	r.HandleFunc("/callback", handleCallback)
+	r.HandleFunc("/authTest", authTest)
 	r.HandleFunc("/lists", getLists).Methods("GET")
 	r.HandleFunc("/lists", createList).Methods("POST")
 	r.HandleFunc("/lists/{ListID}/tasks", getTasks).Methods("GET")
@@ -201,7 +280,7 @@ func handler() {
 	r.HandleFunc("/lists/{ListID}/tasks/{TaskID}", deleteTask).Methods("DELETE")
 
 	// Start server
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 // Main function
